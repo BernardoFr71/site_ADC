b6b18330491cf0ec9fa0b43095bc6c968f182601
Bernardo Freitas <a79295@ualg.pt>
2023-12-04 11:31:25 +0000

    Atualizaçao do site para as faturas

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
             items: [
               {
                 label: 'Faturas',
-                to: 'faturas',
+                to: '/faturas',
               },
             ],
           },
